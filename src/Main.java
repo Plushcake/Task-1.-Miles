552c63a43dc912d1d6392b5a434784b9043f1e3e
Plushcake <nikkolo.makeawelli@gmail.com>
2022-03-16 23:43:35 +0300

    added gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int bilet = 600; //Стоимость билета.
+        int raschet = 20; // Одна миля, двадцать рублей.
+
+        int itog = bilet / raschet;
+        System.out.println("Начисленно милей " + itog);
+    }
+}
